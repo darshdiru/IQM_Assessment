d9ef2c75762aa1197cf418f664f7a6bfd55c1765
Dharshan <48285838+darshdiru@users.noreply.github.com>
2020-09-25 14:10:59 +0530

    Add files via upload
    Updated build folder to be ready for deploying with server side rendered using express.

---
+++ build/precache-manifest.c82974151d2822c3a6071741d5955e7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52af67d4ba92cdbcbc1c3523279b0c73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df3a0331dcefb1a51760",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "74af1dec610b1da471a2",
+    "url": "/static/css/main.84ce41ab.chunk.css"
+  },
+  {
+    "revision": "df3a0331dcefb1a51760",
+    "url": "/static/js/2.54dbda36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.54dbda36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74af1dec610b1da471a2",
+    "url": "/static/js/main.056967f6.chunk.js"
+  },
+  {
+    "revision": "e551d071a655fcb07522",
+    "url": "/static/js/runtime-main.6d392696.js"
+  }
+]);
